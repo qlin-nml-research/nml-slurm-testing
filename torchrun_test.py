a484f7b3a11c1ed70dd4dfaa6a08d76171fad7ea
qlin960618 <qlin1806@gmail.com>
2023-12-05 15:54:45 +0900

    simple test

--- torchrun_test.py
+++ torchrun_test.py
@@ -1,18 +1,18 @@
 import torch
-import torch.distributed as dist
+# import torch.distributed as dist
 
 
 def main():
     # Initialize the distributed environment.
-    dist.init_process_group(backend='nccl')
+    # dist.init_process_group(backend='nccl')
 
     # Get the current rank (node/process number in the cluster)
-    rank = dist.get_rank()
+    # rank = dist.get_rank()
 
     # Get the number of GPUs available on the current node
     num_gpus = torch.cuda.device_count()
 
-    print(f"Rank {rank} has {num_gpus} CUDA devices available.")
+    print(f"Rank {1} has {num_gpus} CUDA devices available.")
 
 
 if __name__ == "__main__":
